c65cfdbfc11ce388c477ac9e5617d10398116926
theKwabena <abbeyemmanueltetteh@gmail.com>
2022-09-25 20:23:46 +0000

    updated asgi

--- core/asgi.py
+++ core/asgi.py
@@ -1,23 +1,18 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 
+
+import os
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-
-import os
-import django
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-# from channels.auth import AuthMiddlewareStack
-from notifications.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
+
+from notifications.routing import websocket_urlpatterns
+
 
 
 application = ProtocolTypeRouter({
